a8901595059a1d3890352d6c699ed6829b50f6ec
Evgeniya <eva.goddess@mail.ru>
2023-09-07 21:47:27 +0200

    HW13: Objects' methods for checking

--- src/pro/sky/java/course1/lesson12/Author.java
+++ src/pro/sky/java/course1/lesson12/Author.java
@@ -3,6 +3,7 @@ package pro.sky.java.course1.lesson12;
 public class Author {
     private String nameFirst;
     private String nameSecond;
+    private String fullName;
 
     public Author(String nameFirst, String nameSecond) {
         this.nameFirst = nameFirst;
@@ -23,4 +24,24 @@ public class Author {
         this.nameSecond = nameSecond;
     }
 
+    public String toString() {
+        return  this.nameFirst + " " + this.nameSecond  + ";";
+
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+        return false;
+        }
+        Author author2 = (Author) other;
+        return nameFirst.equals(author2.nameFirst) && nameSecond.equals(author2.nameSecond) ;
+    }
+
+    @Override
+    public int hashCode() {
+        fullName = nameFirst + nameSecond;
+        return java.util.Objects.hash(fullName);
+    }
 }
+
--- src/pro/sky/java/course1/lesson12/Book.java
+++ src/pro/sky/java/course1/lesson12/Book.java
@@ -5,6 +5,8 @@ public class Book {
     private Author author;
     private int publishingYear;
 
+    private String fullInfo;
+
     public Book(String name, Author author, int publishingYear ) {
         this.name = name;
         this.author = author;
@@ -30,4 +32,24 @@ public class Book {
     public void setPublishingYear(int publishingYear) {
         this.publishingYear = publishingYear;
     }
+
+    public String toString() {
+        return "Имя: " + this.name + "; Автор: " + author.toString() + " Год издания: " + this.publishingYear;
+
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Book book2 = (Book) other;
+        return name.equals(book2.name) && author.equals(book2.author) ;
+    }
+
+    public int hashCode() {
+        fullInfo = name + author.toString() + publishingYear;
+        return java.util.Objects.hash(fullInfo);
+    }
+
 }
